6a4fd2c947fe679639392ea6929c711557f4c425
Jorge Sáez Gómez <josago@gmail.com>
2014-03-04 09:58:40 +0100

    Added NeuralNet template to the TAMER framework.

---
+++ tamer-share/src/ataa2014/NeuralNet.java
@@ -0,0 +1,58 @@
+package ataa2014;
+
+import edu.utexas.cs.tamerProject.modeling.Sample;
+import edu.utexas.cs.tamerProject.modeling.templates.RegressionModel;
+
+/**
+ * This class implements a neural network to be used as a regression model to learn the H function within the TAMER framework.
+ * @author josago
+ */
+public class NeuralNet extends RegressionModel
+{
+	public NeuralNet(int num_inputs, int num_hidden)
+	{
+		// TODO Initialize the neural network accordingly.
+	}
+	
+	@Override
+	public void addInstance(Sample sample)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addInstances(Sample[] samples)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addInstancesWReplacement(Sample[] samples)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buildModel()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double predictLabel(double[] feats)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clearSamplesAndReset()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
