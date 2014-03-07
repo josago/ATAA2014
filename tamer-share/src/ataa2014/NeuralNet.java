79e7e7e1068f86146f811da84b17c7f1d0b8d1df
Jorge Sáez Gómez <josago@gmail.com>
2014-03-07 16:55:16 +0100

    We improved the implementation of NeuralNet and now it works better. We also got rid of the annoying tile-printing.

--- tamer-share/src/ataa2014/NeuralNet.java
+++ tamer-share/src/ataa2014/NeuralNet.java
@@ -1,10 +1,11 @@
 package ataa2014;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
+import org.neuroph.core.transfer.TransferFunction;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.learning.LMS;
 import org.neuroph.util.TransferFunctionType;
@@ -20,47 +21,74 @@ public class NeuralNet extends RegressionModel
 {
 	private ArrayList <Double> outputList;
 	private ArrayList <double[]> sampleList;
-	
-	private boolean newSamples;
 
-	private NeuralNetwork neuralNet;
+	private MultiLayerPerceptron neuralNet;
 	
-	private int num_inputs;
-	private int num_hidden;
+	private final int num_inputs;
+	private final int num_hidden;
 	
-	private int maxIterations = 100;
+	private int maxIterations = Integer.MAX_VALUE;
+	
+	private class TransferFunctionCustom extends TransferFunction
+	{
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public double getOutput(double net)
+		{
+			return 10 * Math.sin(net);
+		}
+		
+		@Override
+		public double getDerivative(double net)
+		{
+			return 10 * Math.cos(net);
+		}
+	}
 	
 	public NeuralNet(int num_inputs, int num_hidden)
 	{
-		
 		this.num_inputs = num_inputs;
 		this.num_hidden = num_hidden;
 		
 		outputList = new ArrayList <Double>();
 		sampleList = new ArrayList <double[]>();
 		
-		neuralNet = new MultiLayerPerceptron(TransferFunctionType.TANH, num_inputs, num_hidden, 1);
-
+		neuralNet = new MultiLayerPerceptron(TransferFunctionType.SIGMOID, num_inputs, num_hidden, 1);
+		neuralNet.randomizeWeights();
+		
+		neuralNet.getLayerAt(2).getNeuronAt(0).setTransferFunction(new TransferFunctionCustom());
+		
 	    ((LMS)neuralNet.getLearningRule()).setMaxError(0.001);//0-1
-	    ((LMS)neuralNet.getLearningRule()).setLearningRate(0.001);//0-1
-	    ((LMS)neuralNet.getLearningRule()).setMaxIterations(maxIterations);//0-1
-	    
+	    ((LMS)neuralNet.getLearningRule()).setLearningRate(0.000001);//0-1
+	    ((LMS)neuralNet.getLearningRule()).setMaxIterations(maxIterations);//0-1 
 	}
 	
 	@Override
 	public void addInstance(Sample sample)
-	{	
-		double[] attributes = sample.getAttributes(); // features + label as last element
-		double[] features = new double[attributes.length - 1];
-		
-		for (int i = 0; i < attributes.length-1 ; i++){
-			features[i] = attributes[i];
+	{
+		if (sample.label != 0.0)
+		{
+			boolean found = false;
 			
+			for (int i = 0; i < sampleList.size(); i++)
+			{
+				if (Arrays.equals(sample.feats, sampleList.get(i)))
+				{
+					found = true;
+
+					outputList.set(i, (outputList.get(i) + sample.label) / 2);
+				}
+			}
+			
+			if (!found)
+			{
+				sampleList.add(sample.feats);
+				outputList.add(sample.label);
+			}
+			
+			System.err.println("NN adding instance " + sample.label + " to a list of size " + sampleList.size());
 		}
-		sampleList.add(features);
-		outputList.add(attributes[attributes.length - 1]); 
-		
-		
 	}
 
 	@Override
@@ -86,15 +114,21 @@ public class NeuralNet extends RegressionModel
 	public void buildModel()
 	{
 		DataSet trainingSet = new DataSet(num_inputs, 1);	
-		//System.out.println("data size:"+ sampleList.size() );
+		
 		for (int i = 0; i < sampleList.size(); i++){
 			trainingSet.addRow(new DataSetRow(sampleList.get(i), new double[]{outputList.get(i)}));
 		}
 	   
-	    neuralNet.learn(trainingSet);
+	    neuralNet.learnInNewThread(trainingSet);
 	    
-	    // return neuralNet.getOutput()[0];
-		
+	    try {
+			Thread.sleep(1000 / 24);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    neuralNet.stopLearning();
 	}
 
 	@Override
@@ -102,6 +136,7 @@ public class NeuralNet extends RegressionModel
 	{	
 		neuralNet.setInput(feats);
 		neuralNet.calculate();
+		System.out.println("NN output: " + neuralNet.getOutput()[0]);
 		return neuralNet.getOutput()[0];
 	}
 
@@ -111,8 +146,7 @@ public class NeuralNet extends RegressionModel
 		outputList = new ArrayList <Double>();
 		sampleList = new ArrayList <double[]>();
 		
-		
-		neuralNet = new MultiLayerPerceptron(TransferFunctionType.TANH, num_inputs, num_hidden, 1);
+		neuralNet = new MultiLayerPerceptron(TransferFunctionType.LINEAR, num_inputs, num_hidden, 1);
 
 	    ((LMS)neuralNet.getLearningRule()).setMaxError(0.001);//0-1
 	    ((LMS)neuralNet.getLearningRule()).setLearningRate(0.001);//0-1
